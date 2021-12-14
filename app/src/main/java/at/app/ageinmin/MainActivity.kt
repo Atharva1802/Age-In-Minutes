6ecbdd16e4136722cfca591ab96632bd2aee9b69
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2021-12-14 20:57:41 +0530

    Button issue resolved

--- app/src/main/java/at/app/ageinmin/MainActivity.kt
+++ app/src/main/java/at/app/ageinmin/MainActivity.kt
@@ -2,13 +2,15 @@ package at.app.ageinmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        btnDatePicker.setOnClickListener{Toast.makeText(this, "button works",Toast.LENGTH_LONG)show()}
 
     }
 }
