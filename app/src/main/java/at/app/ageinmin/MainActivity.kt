e803ed664de97d7099a6633d2c987b7dd51e5ba3
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2021-12-15 13:15:55 +0530

    Added myCalendar

--- app/src/main/java/at/app/ageinmin/MainActivity.kt
+++ app/src/main/java/at/app/ageinmin/MainActivity.kt
@@ -1,16 +1,40 @@
 package at.app.ageinmin
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnDatePicker.setOnClickListener{Toast.makeText(this, "button works",Toast.LENGTH_LONG)show()}
+
+        btnDatePicker.setOnClickListener {view ->
+            clickDatePicker(view)}
+
+
+    }
+    fun clickDatePicker(view: View) {
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener {
+                    view, year, month, dayOfMonth ->
+            },
+            year,
+            month,
+            day,
+        ).show()
 
     }
 }
